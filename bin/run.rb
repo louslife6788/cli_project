f429a2bff8b288c945e3879a2cb3d0e162acf415
luis leon <leonluis380@yahoo.com>
2021-08-30 09:31:05 -0400

    car/names

--- bin/run.rb
+++ bin/run.rb
@@ -13,8 +13,12 @@ enviorment
 
 
 
-page_scrape = Scrape.new("https://www.autoevolution.com/audi/")
+page_audi = Scrape.new("https://www.autoevolution.com/audi/")
+page_honda = Scrape.new("https://www.autoevolution.com/honda/")
 
-x = page_scrape.get_body
+y = page_honda.honda_selector
+
+
+x = page_audi.audi_selector
 binding.pry
 
--- lib/Scrape.rb
+++ lib/Scrape.rb
@@ -20,18 +20,34 @@ class Scrape
     end
 
 
-    def selector
-        get_body.css()
-        
+    def audi_selector
+      audi_colum =  get_body.css("div.carmodels.col23width.clearfix")
+      audi_colum.map.with_index(1) do |audi|
+         y = audi.css("div.carmod.clearfix")
+         y.map do |name|
+            name.css("h4").text
+            
+         end
+      end
     end
+
+    def honda_selector
+        honda_colum =  get_body.css("div.carmodels.col23width.clearfix")
+        honda_colum.map.with_index(1) do |honda|
+           y = honda.css("div.carmod.clearfix")
+           y.map do |name|
+              name.css("h4").text
+              
+           end
+        end
+      end
+
+
+
+
 
 
  
-
-
-
-
-
 
 end
 
