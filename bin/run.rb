4f507684a78e196f2991022dda458040535e5e73
luis leon <leonluis380@yahoo.com>
2021-09-02 21:11:42 -0400

    organized info to hashes

--- bin/run.rb
+++ bin/run.rb
@@ -18,9 +18,8 @@ page_audi = Scrape.new("https://www.autoevolution.com/audi/")
 page_honda = Scrape.new("https://www.autoevolution.com/honda/")
 page_chevy = Scrape.new("https://www.autoevolution.com/chevrolet/")
 
-brands = car_brand.car_brands
-honda = page_honda.info_selector
-chevy = page_chevy.info_selector
-audi = page_audi.info_selector
+# brands = car_brand.car_brands
+ honda = page_honda.info_selector
+# chevy = page_chevy.info_selector
+# audi = page_audi.info_selector
 
-puts brands
--- lib/Scrape.rb
+++ lib/Scrape.rb
@@ -21,15 +21,18 @@ class Scrape
 
 
     def info_selector
+        models = []
       car_models =  get_body.css("div.carmodels.col23width.clearfix")
       car_models.map.with_index(1) do |car|
          y = car.css("div.carmod.clearfix")
          y.map do |car_data|
-            car_data.css("h4").text
-            
-            
-         end
+            name = car_data.css("h4").text
+            link = car_data.css("a").first["href"]
+            models << {name: name, link: link}
+            # binding.pry
+        end
       end
+     puts models
     end
 
     def car_brands
@@ -38,15 +41,13 @@ class Scrape
         brand.map do |brands|
             y = brands.css("div.col2width.fl.bcol-white.carman")
             y.map do |brand_info|
-                 options << brand_info.css("span").text
-                 options << brand_info.css("a").first["href"]
+                 name =  brand_info.css("span").text
+                 link = brand_info.css("a").first["href"]
+                 options << {name: name, link: link}
                 
-                
-
-
             end
         end
-        options
+        puts options
     end
 
 
